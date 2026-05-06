393cd9b37481f4d98bd76de21bd435871006754d
Debayan Sarkar <139753774+d3byn@users.noreply.github.com>
2026-05-06 15:40:59 +0000

    learnt datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello")  //we are using nodejs, not browser
+
+let name = "Deb"
+let age = 20
+let isLoggedIn = true
+
+/*
+number = 2 ^ 53 (range)
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+*/
+
+console.log(typeof undefined) //undefined
+console.log(typeof "deb"); //string
+console.log(typeof null); //object
