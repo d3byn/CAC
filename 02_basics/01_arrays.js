f3b4770d20d4ce1fa4e851ebe174a3393d597e23
Debayan Sarkar <139753774+d3byn@users.noreply.github.com>
2026-05-22 10:32:53 +0000

    learnt arrays 01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["iron-man", "spiderman"];
+
+const myArr2 = new Array(1, 3, 5, 7);
+
+//console.log(myArr)
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+//myArr.unshift(9)
+//myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(4))
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr)
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("C ", myArr);
+console.log(myn2);
+
+// 1.Push = adds value to last of an array.
+// 2. Pop = removes last value of an array.
+// 3. Unshift = adds value at first of an array.
+// 4.Shift = removes first value of an array.
+// 5. Includes = checks true or false.
+// 6. IndexOf = checks the position of value in number form.
+// 7.Join = converts to string.
+// 8. Slice(value to remove from,value remove upto but don't include)= returns new array and original value remains unchanged. Use when you need to extract a portion of an array without modifying it.
+// 9. Splice(value to delete from, number of value to delete) = returns new array with deleted one and original value changed.Use when you need to add, remove, or replace elements within an array.
