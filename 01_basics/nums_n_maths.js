85df20fa3d2dafbbe6c9d9634c1cde61987e414b
Debayan Sarkar <139753774+d3byn@users.noreply.github.com>
2026-05-22 09:31:32 +0000

    learnt math fnc

---
+++ 01_basics/nums_n_maths.js
@@ -0,0 +1,34 @@
+const score = 200
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// +++++++++++++++ maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,3,6,7,2,34));
+
+console.log(Math.random()) // always give random number between 0 and 1
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min)
+
